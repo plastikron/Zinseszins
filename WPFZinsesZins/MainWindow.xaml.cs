9d312419d899031e1dee02045f86cb6d9df90a5c
plastikron <thyenkay@dv-schulen.de>
2018-04-18 11:48:52 +0200

    geht

---
+++ WPFZinsesZins/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace WPFZinsesZins
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ZinsesZins z1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            z1 = new ZinsesZins();
+            z1.A_kapital = 1000;
+            z1.Zins = 10;
+            z1.Jahre = 5;
+            this.DataContext = z1;
+
+        }
+
+        private void b_Berechnen_Click(object sender, RoutedEventArgs e)
+        {
+            //z1 = new ZinsesZins(Convert.ToDouble(t_Kapitalwert.Text),Convert.ToDouble(t_Zinsfusswert.Text),Convert.ToDouble(t_Laufzeitwert.Text));
+
+            z1.BerechneKapital();
+
+
+        }
+    }
+}
---
+++ WPFZinsesZins/ZinsesZins.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WPFZinsesZins
+{
+    class ZinsesZins : INotifyPropertyChanged
+    {
+        private Double a_kapital;
+        private Double e_kapital;
+        private Double zins;
+        private Double jahre;
+        private Double gewinn;
+        private Double prozent;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+        public double A_kapital
+        {
+            get
+            {
+                return a_kapital;
+            }
+
+            set
+            {
+                a_kapital = value;
+            }
+        }
+
+        public double E_kapital
+        {
+            get
+            {
+                return e_kapital;
+            }
+
+            private set
+            {
+                e_kapital = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("E_kapital"));
+            }
+        }
+
+        public double Zins
+        {
+            get
+            {
+                return zins;
+            }
+
+            set
+            {
+                zins = value;
+            }
+        }
+
+        public Double Jahre
+        {
+            get
+            {
+                return jahre;
+            }
+
+            set
+            {
+                jahre = value;
+            }
+        }
+
+        public double Gewinn
+        {
+            get
+            {
+                return gewinn;
+            }
+
+            private set
+            {
+                gewinn = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Gewinn"));
+            }
+        }
+
+        public double Prozent
+        {
+            get
+            {
+                return prozent;
+            }
+
+            private set
+            {
+                prozent = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Prozent"));
+            }
+        }
+
+        public ZinsesZins(Double a_kap, Double zinsfuss, Double laufzeit)
+        {
+            A_kapital = a_kap;
+            Zins = zinsfuss;
+            Jahre = laufzeit;
+        }
+        public ZinsesZins()
+        {
+
+        }
+
+        public void BerechneKapital()
+        {
+            E_kapital = a_kapital * Math.Pow((1 + zins / 100), Jahre);
+            Gewinn = E_kapital - A_kapital;
+            Prozent = Gewinn / A_kapital * 100;
+        }
+    }
+}
